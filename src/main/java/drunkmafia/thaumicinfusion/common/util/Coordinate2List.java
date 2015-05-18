a5e5d29f4148181f65e45821003b6e6203ce476a
TheDrunkMafia <TheDrunkMafia@users.noreply.github.com>
2015-05-18 12:17:27 +0100

    Update Coordinate2List.java

--- src/main/java/drunkmafia/thaumicinfusion/common/util/Coordinate2List.java
+++ src/main/java/drunkmafia/thaumicinfusion/common/util/Coordinate2List.java
@@ -2,19 +2,52 @@ package drunkmafia.thaumicinfusion.common.util;
 
 import java.lang.reflect.Array;
 
+/**
+ * This works based on a graph design to allow for negative coordinates:
+ *          X
+ *          |
+ *          |
+ *   PosNeg | PosPos
+ *  ------------------- Z
+ *   NegNeg | NegPos
+ *          |
+ *          |
+ * Making it ideal for coordinate based lists, it gives a faster lookup time than lists and maps
+ * since you need to know the coordinates of the element you want to look up.
+ *
+ * This also works like the list, it will automatically resize itself to fit elements. Run the clean
+ * every so often to trim the arrays.
+ *
+ * @author TheDrunkMafia
+ */
 @SuppressWarnings("unchecked")
-public class Coordinate2List<T> {
+public class Coordinate2List<T>{
 
-    private T[][] posPos, negNeg, posNeg, negPos;
+    /**
+     * The array buffer into which the elements of the ArrayList are stored.
+     */
+    private transient T[][] posPos, negNeg, posNeg, negPos;
 
+    private int size, initalSize;
+
+    /**
+     * The margin which the arrays are shifted by when being resized
+     */
+    private int shitMargin;
     private Class<T> tClass;
 
     /**
      * @param initalSize of arrays, this will increase put Times
      * @param tClass used to create the arrays
+     * @param shitMargin used to push the array size up to give some leeway
      */
-    public Coordinate2List(Class<T> tClass, int initalSize){
+    public Coordinate2List(Class<T> tClass, int initalSize, int shitMargin){
+        if(tClass == null || initalSize < 0 || shitMargin < 0)
+            throw new IllegalArgumentException("Bad Arguments, Failed to create list. Class: " + tClass + " Size: " + initalSize + " Shift: " + shitMargin);
+
         this.tClass = tClass;
+        this.shitMargin = shitMargin;
+        this.initalSize = initalSize;
 
         posPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
         negNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
@@ -23,7 +56,7 @@ public class Coordinate2List<T> {
     }
 
     public Coordinate2List(Class<T> tClass){
-        this(tClass, 1);
+        this(tClass, 1, 100);
     }
 
     public Coordinate2List(){
@@ -44,9 +77,10 @@ public class Coordinate2List<T> {
         z = Math.abs(z);
 
         T[][] array = (xPos && zPos) ? posPos : (!xPos && zPos) ? negPos : (xPos && !zPos) ? posNeg : negNeg;
-        if(x >= array.length) array = changeArraySize(array, x + 100);
-        if(z >= array[x].length) array[x] = changeArraySize(array[x], z + 100);
+        if(x >= array.length) array = changeArraySize(array, x + shitMargin);
+        if(z >= array[x].length) array[x] = changeArraySize(array[x], z + shitMargin);
 
+        size += element != null ? 1 : -1;
         array[x][z] = element;
 
         if(xPos && zPos) posPos = array;
@@ -68,6 +102,65 @@ public class Coordinate2List<T> {
 
         T[][] array = (xPos && zPos) ? posPos : (!xPos && zPos) ? negPos : (xPos && !zPos) ? posNeg : negNeg;
         return (x < array.length && z < array[x].length) ? tClass.cast(array[x][z]) : null;
+    }
+
+    public void cleanAll(){
+        posPos = clean(posPos);
+        negNeg = clean(negNeg);
+        negPos = clean(negPos);
+        posNeg = clean(posNeg);
+    }
+
+    private T[][] clean(T[][] old){
+        for(int x = 0; x < old.length; x++){
+            int lastZ = 0;
+
+            T[] array = old[x];
+            for(int z = 0; z < old[x].length; z++){
+                T obj = array[z];
+                if(obj != null)
+                    lastZ = z;
+            }
+
+            if(lastZ != array.length - 1)
+                old[x] = changeArraySize(old[x], lastZ);
+        }
+        return old;
+    }
+
+    public void remove(int x, int z){
+        set(null, x, z);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " - Size: " + size + " Type: " + tClass.getSimpleName();
+    }
+
+    /**
+     * Recreates the list, removing all stored objects
+     */
+    public void removeAll(){
+        size = 0;
+
+        posPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
+        negNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
+        posNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
+        negPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
+    }
+
+    /**
+     * @return is the list empty
+     */
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * @return Total size of the list
+     */
+    public int size(){
+        return size;
     }
 
     public T[][] getPosPos(){
