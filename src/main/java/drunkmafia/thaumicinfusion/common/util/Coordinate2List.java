a23353e604d5c825648ffc3ee85baf31d48cc956
TheDrunkMafia <TheDrunkMafia@users.noreply.github.com>
2015-06-04 12:16:07 +0100

    Fixed duping Bug
    The issue I have been trying to resolve for the past month is finally done, now Thaumic Infusion will be able to run faster than minecrafts own chunk system.

--- src/main/java/drunkmafia/thaumicinfusion/common/util/Coordinate2List.java
+++ src/main/java/drunkmafia/thaumicinfusion/common/util/Coordinate2List.java
@@ -1,8 +1,6 @@
 package drunkmafia.thaumicinfusion.common.util;
 
 import java.lang.reflect.Array;
-import java.util.*;
-import java.util.function.Consumer;
 
 /**
  * This works based on a graph design to allow for negative coordinates:
@@ -23,15 +21,16 @@ import java.util.function.Consumer;
  * @author TheDrunkMafia
  */
 @SuppressWarnings("unchecked")
-public class Coordinate2List<T> implements Iterable<T>, RandomAccess, java.io.Serializable{
+public class Coordinate2List<T> {
 
     /**
      * The array buffer into which the elements of the ArrayList are stored.
      */
-    private transient T[][] posPos, negNeg, posNeg, negPos;
+    private Integer[][] posPos, negNeg, posNeg, negPos;
 
-    private ArrayList<T> elements;
     private int initalSize;
+
+    private T[] elementData;
 
     /**
      * The margin which the arrays are shifted by when being resized
@@ -43,25 +42,22 @@ public class Coordinate2List<T> implements Iterable<T>, RandomAccess, java.io.Se
      * @param initalSize of arrays, this will increase put Times
      * @param tClass used to create the arrays
      * @param shitMargin used to push the array size up to give some leeway
+     * @param maxAttempts used to ensure that the list does not cause an infinite loop when adding data
      */
-    public Coordinate2List(Class<T> tClass, int initalSize, int shitMargin){
+    public Coordinate2List(Class<T> tClass, int initalSize, int shitMargin, int maxAttempts){
         if(tClass == null || initalSize < 0 || shitMargin < 0)
             throw new IllegalArgumentException("Bad Arguments, Failed to create list. Class: " + tClass + " Size: " + initalSize + " Shift: " + shitMargin);
 
         this.tClass = tClass;
         this.shitMargin = shitMargin;
         this.initalSize = initalSize;
+        this.maxAttempts = maxAttempts;
 
-        elements = new ArrayList(initalSize);
-
-        posPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        negNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        posNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        negPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
+        removeAll();
     }
 
     public Coordinate2List(Class<T> tClass){
-        this(tClass, 1, 100);
+        this(tClass, 1, 100, 5);
     }
 
     public Coordinate2List(){
@@ -78,20 +74,39 @@ public class Coordinate2List<T> implements Iterable<T>, RandomAccess, java.io.Se
      */
     public void set(T element, int x, int z){
         boolean xPos = x > 0, zPos = z > 0;
+        Integer[][] array = getArray(xPos, zPos);
 
-        x = (z < 0) ? -z : z;
+        x = (x < 0) ? -x : x;
         z = (z < 0) ? -z : z;
 
-        T[][] array = getArray(xPos, zPos);
+        if(x >= array.length) array = changeArraySize(array, x + shitMargin, new Integer[x + shitMargin], Integer.class);
+        if(array[x] != null && z >= array[x].length) array[x] = changeArraySize(array[x], z + shitMargin, Integer.class);
 
-        if(x >= array.length) array = changeArraySize(array, x + shitMargin);
-        if(z >= array[x].length) array[x] = changeArraySize(array[x], z + shitMargin);
-
-        if(element != null) elements.add(element);
-        else elements.remove(array[x][z]);
-        array[x][z] = element;
+        if(element != null)
+            array[x][z] = addElement(element);
+        else{
+            elementData[array[x][z]] = null;
+            array[x][z] = null;
+        }
 
         setArray(xPos, zPos, array);
+    }
+
+    private int attempt = 0, maxAttempts;
+
+    public int addElement(T element){
+        if(attempt++ > maxAttempts)
+            throw new IllegalArgumentException("Exceeded max amount of attempts to add data to list");
+
+        for(int i = 0; i < elementData.length; i++){
+            if(elementData[i] == null){
+                elementData[i] = element;
+                attempt = 0;
+                return i;
+            }
+        }
+        elementData = changeArraySize(elementData, elementData.length + shitMargin + 1, tClass);
+        return addElement(element);
     }
 
     /**
@@ -101,85 +116,39 @@ public class Coordinate2List<T> implements Iterable<T>, RandomAccess, java.io.Se
      * @return Element at that position, can be null
      */
     public T get(int x, int z){
-        boolean xPos = x > 0, zPos = z > 0;
+        Integer[][] array = getArray(x > 0, z > 0);
 
-        x = (z < 0) ? -z : z;
+        x = (x < 0) ? -x : x;
         z = (z < 0) ? -z : z;
-
-        T[][] array = (xPos && zPos) ? posPos : (!xPos && zPos) ? negPos : (xPos && !zPos) ? posNeg : negNeg;
-        return (x < array.length && z < array[x].length) ? array[x][z] : null;
+        return (x < array.length && z < array[x].length && array[x][z] != null) ? elementData[array[x][z]] : null;
     }
 
-    private T[][] getArray(boolean xPos, boolean zPos){
+    private Integer[][] getArray(boolean xPos, boolean zPos){
         return xPos && zPos ? posPos : !xPos && zPos ? negPos : xPos ? posNeg : negNeg;
     }
 
-    private void setArray(boolean xPos, boolean zPos, T[][] array){
+    private void setArray(boolean xPos, boolean zPos, Integer[][] array){
         if(xPos && zPos) posPos = array;
         if(!xPos && zPos) negPos = array;
         if(xPos && !zPos) posNeg = array;
         if(!xPos && !zPos) negNeg = array;
     }
 
-    public void cleanAll(){
-        elements = new ArrayList(initalSize);
-
-        posPos = clean(posPos);
-        negNeg = clean(negNeg);
-        negPos = clean(negPos);
-        posNeg = clean(posNeg);
-    }
-
-    private T[][] clean(T[][] old){
-        for(int x = 0; x < old.length; x++){
-            int lastZ = 0;
-
-            T[] array = old[x];
-            for(int z = 0; z < old[x].length; z++){
-                T obj = array[z];
-                if(obj != null)
-                    lastZ = z;
-            }
-
-            if(lastZ != array.length - 1)
-                old[x] = changeArraySize(old[x], lastZ);
-        }
-        return old;
-    }
-
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " - Size: " + elements.size() + " Type: " + tClass.getSimpleName();
+        return getClass().getSimpleName() + " Type: " + tClass.getSimpleName();
     }
 
     /**
      * Recreates the list, removing all stored objects
      */
     public void removeAll(){
-        elements = new ArrayList<>();
+        elementData = (T[]) Array.newInstance(tClass, initalSize, initalSize);
 
-        posPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        negNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        posNeg =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-        negPos =  (T[][]) Array.newInstance(tClass, initalSize, initalSize);
-    }
-
-    /**
-     * @return is the list empty
-     */
-    public boolean isEmpty(){
-        return elements.size() == 0;
-    }
-
-    /**
-     * @return Total size of the list
-     */
-    public int size(){
-        return elements.size();
-    }
-
-    public ArrayList<T> getElements(){
-        return elements;
+        posPos = new Integer[initalSize][initalSize];
+        negNeg = new Integer[initalSize][initalSize];
+        posNeg = new Integer[initalSize][initalSize];
+        negPos = new Integer[initalSize][initalSize];
     }
 
     /**
@@ -188,73 +157,22 @@ public class Coordinate2List<T> implements Iterable<T>, RandomAccess, java.io.Se
      * @param newSize of the array to create
      * @return New array with old elements and new size
      */
-    protected T[] changeArraySize(T[] old, int newSize){
-        T[] newArray = (T[]) new Object[newSize];
-        for(int i = 0; i < newSize; i++){
-            if(i < old.length)
-                newArray[i] = old[i];
-        }
+    protected <E>E[] changeArraySize(E[] old, int newSize, Class<E> type){
+        E[] newArray = (E[]) Array.newInstance(type, newSize);
+        System.arraycopy(old, 0, newArray, 0, old.length);
         return newArray;
     }
-
     /**
      * Resize a 2 dimensional array
      * @param old array
      * @param newSize of the array to create
      * @return New array with old elements and new size
      */
-    protected T[][] changeArraySize(T[][] old, int newSize){
-        T[][] newArray = (T[][]) new Object[newSize][newSize];
+    protected <E>E[][] changeArraySize(E[][] old, int newSize, E[] defaultVal, Class<E> type){
+        E[][] newArray = (E[][]) Array.newInstance(type, newSize, newSize);
         System.arraycopy(old, 0, newArray, 0, old.length);
+        for(int i = 0; i < newArray.length; i++)
+            if(newArray[i] == null) newArray[i] = defaultVal;
         return newArray;
-    }
-
-    public Iterator<T> iterator() {
-        return new Itr();
-    }
-
-    private class Itr implements Iterator<T> {
-        int cursor;
-        int lastRet = -1;
-
-        public T[] elementData;
-
-        public Itr(){
-            elementData = (T[]) Coordinate2List.this.elements.toArray();
-        }
-
-        public boolean hasNext() {
-            return cursor != elementData.length;
-        }
-
-        @SuppressWarnings("unchecked")
-        public T next() {
-            int i = cursor;
-            if (i >= elementData.length)
-                throw new NoSuchElementException();
-            cursor = i + 1;
-            return elementData[lastRet = i];
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public void forEachRemaining(Consumer<? super T> consumer) {
-            Objects.requireNonNull(consumer);
-            final int size = elementData.length;
-            int i = cursor;
-            if (i >= size)
-                return;
-
-            if (i >= elementData.length)
-                throw new ConcurrentModificationException();
-
-            // update once at end of iteration to reduce heap write traffic
-            cursor = i;
-            lastRet = i - 1;
-        }
     }
 }
